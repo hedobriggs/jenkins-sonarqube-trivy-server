e9a9b3c3f4eb7436301df238e5744ed17e592441
hedobriggs <79212477+hedobriggs@users.noreply.github.com>
2026-02-02 11:18:21 +0000

    Update userdata.sh with Java version 21

--- userdata.sh
+++ userdata.sh
@@ -9,7 +9,8 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-sudo dnf install java-17-amazon-corretto -y
+sudo dnf install java-21-amazon-corretto -y
+sudo alternatives --set java /usr/lib/jvm/java-21-amazon-corretto/bin/java
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
